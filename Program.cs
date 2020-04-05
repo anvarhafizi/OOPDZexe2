33c97d54688ffb4345cfb8d6ff7e5b4d7075a7ec
unknown <yourGitHub@example.com>
2020-04-05 04:04:25 +0300

    Added public(Rectangle,Area,Perimetr)

--- Program.cs
+++ Program.cs
@@ -3,7 +3,35 @@
 namespace ConsoleAPlication
 {
     class Rectangle
-    {
+    {   
+        double side1,side2;
+        public Rectangle(double side1, double side2)
+        {
+            this.side1=side1;
+            this.side2=side2;
+        }
+        double AreaCalculator()
+        {
+            return side1*side2;
+        }
+        double PerimetrCalculator()
+        {
+            return 2*(side1+side2);
+        }
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimetr
+        {
+            get
+            {
+                return PerimetrCalculator();
+            }
+        }
         static void Main(string[] args)
         {
            
